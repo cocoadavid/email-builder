1326ef2f59a721ce7864f361131429e6d88ed1cc
Szilárd Dávid <szilard.david91@gmail.com>
2025-08-16 21:48:40 +0200

    fixed typo

--- src/components/emailComponents/SectionWithBgImg.tsx
+++ src/components/emailComponents/SectionWithBgImg.tsx
@@ -22,7 +22,7 @@ const SectionWithBgImg = ({
   mobileContent,
 }: SectionWithBgImgProps) => {
   const widthInPt = `${width * 0.75}pt`;
-  const heightInPt = `${height * 0.75}pt;`;
+  const heightInPt = `${height * 0.75}pt`;
 
   return (
     <section style={{ margin: 0, padding: 0, width: '100%', minWidth: '100%', maxWidth: '100%' }}>
@@ -46,7 +46,7 @@ const SectionWithBgImg = ({
                             <!--[if gte mso 9]>
 <v:image xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style=" border: 0;display: inline-block; width: ${widthInPt}; height: ${heightInPt}; padding: 0; margin: 0;" src="${bgImgSrc}"/>
 <v:rect xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style=" border: 0;display: inline-block;position: absolute; width: ${widthInPt}; height: ${heightInPt}; font-size: 0px; line-height: 0px; padding: 0; margin: 0;">
-<v:fill opacity="0%" color="${bgColor}” style="margin: 0; padding: 0; font-size: 0; font-height: 0; mso-margin-left: 0; mso-margin-right: 0; mso-margin-bottom: 0; mso-margin-top: 0;" />
+<v:fill opacity="0%" color="${bgColor}" style="margin: 0; padding: 0; font-size: 0; font-height: 0; mso-margin-left: 0; mso-margin-right: 0; mso-margin-bottom: 0; mso-margin-top: 0;" />
 <v:textbox inset="0,0,0,0" v-text-anchor="middle">
 <![endif]-->`,
                 }}
